ae1d15ff597da72e30559019802e21eaf703d154
wendell.borges <wendell.borges@startyourapp.com.br>
2024-08-28 20:30:19 -0300

    add success

---
+++ src/app/form/success/page.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import Image from 'next/image';
+
+import { Whatsapp } from '@/components/whatsapp';
+
+import { Button } from '@/components/button';
+import { IconRosetteDiscountCheckFilled } from '@tabler/icons-react';
+import { useRouter } from 'next/navigation';
+
+export default function Success({ params }: { params: { number: string } }) {
+  const router = useRouter();
+  const handleOnClick = () => {
+    router.push('/');
+  };
+  return (
+    <>
+      <nav className="flex w-full justify-center items-center lg:px-24 fixed bg-[#1a1a1b] z-[99] h-[120px] top-0">
+        <Image
+          src={'/izzat.svg'}
+          alt="IzzatSquad"
+          height={53}
+          width={268}
+          className="w-[268px"
+          onClick={handleOnClick}
+        />
+      </nav>
+      <main className="flex min-h-screen flex-col items-center w-full lg:w-[900px] mt-[32px] mx-auto overflow-hidden px-4">
+        <p className="text-white font-medium text-[24] lg:text-[32px]">
+          Formulário enviado com sucesso!
+        </p>
+        <IconRosetteDiscountCheckFilled className="text-[#FFF] lg:w-[160px] lg:h-[160px] mt-[16px] w-[100px] h-[100px]" />
+        <p className="text-white font-medium  text-[18] lg:text-[24px] text-center mt-[16px]">
+          E aguardamos sua presença no evento! e qualquer dúvida entre em
+          contato no nosso whatsapp.
+        </p>
+        <div className="flex w-full justify-center gap-4 mt-[32px]">
+          <Button
+            text="Voltar"
+            variant="contained"
+            onClick={() => router.push('/')}
+          />
+        </div>
+      </main>
+      <Whatsapp />
+    </>
+  );
+}
